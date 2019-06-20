124df8a671d14f0839ace1a9a98a219d3df0f080
Cole Hudson <cole@colejhudson.com>
2019-06-20 01:31:21 -0700

    renamed package

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
 setuptools.setup(
-    name='twitter-to-parquet-transformer',
+    name='hidden-alphabet-transformers-twitter',
     version='0.0.0',
     author=["Cole Hudson", "Chris Louie"],
     author_email="cole@colejhudson.com",
-    description="AWS Lambda function to parse HTML into Parquet",
+    description="AWS Lambda function to parse HTML from twitter.com/search into Parquet",
     install_requires=[
         "bs4",
         "s3fs",
