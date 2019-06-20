83f08f4379e11386157e7969b25397ad6ca26a64
Cole Hudson <cole@colejhudson.com>
2019-06-19 17:48:46 -0700

    automated test data generation

--- profiling/profile.sh
+++ profiling/profile.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
-FILE=$PWD/profiling/example.html
-CMD=$PWD/profiling/test.py
+# DIR variable from https://stackoverflow.com/a/246128/8738498
+DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
+
+CMD=$DIR/test.py
+FILE=$DIR/example.html
+
+STACKTRACE=$DIR/profile.txt
+FLAMEGRAPH=$DIR/flamegraph.svg.html
+
+USERAGENT="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0"
+
+curl \
+  -H "User-Agent: ${USERAGENT}" \
+  "https://twitter.com/search?q=tesla%20lang%3Aen%20since%3A2014-01-01%20&src=typd" \
+  > $FILE
 
 docker run \
     -v $PWD:$PWD \
