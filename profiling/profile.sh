330c19181fb7fe93d470a4ed07789c20ddb7bbd3
Cole Hudson <cole@colejhudson.com>
2019-06-19 17:10:05 -0700

    cleaned up artifacts

---
+++ profiling/profile.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+FILE=$PWD/profiling/example.html
+CMD=$PWD/profiling/test_lambda.py
+
+docker run \
+    -v $PWD:$PWD \
+    --cap-add SYS_PTRACE \
+    html_to_parquet \
+    --flamegraph \
+    -o $PWD/profile.txt \
+    -t python3 $CMD $FILE
+
+docker run flamegraph $PWD/profile.txt > flamegraph.svg
+
+open -a "Google Chrome" flamegraph.svg
